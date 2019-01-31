27a4d76f582775b2092ded10c0dd498cf04d43e6
Jakub Czerwiński <p.jakub.czerwinski@gmail.com>
2019-01-31 11:40:52 +0100

    Really ugly GUI

--- Wordcheck/Wordcheck.py
+++ Wordcheck/Wordcheck.py
@@ -1,20 +1,30 @@
 import gzip
 import itertools
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
 
-class Wordcheck(object):
+#########################################################
+#   Make less ugly gui and mobile friendly              #
+#   Speed word search (split files by first letter)     #
+#   Change palette                                      #
+#########################################################
+class Wordcheck(Widget):
     WORDS_FILE = "words.dat"
+    results = StringProperty()
 
     def perm(self, words, lenght, letters):
         perms = []
+        foundWords = []
         perm = itertools.permutations(letters, lenght)
         for item in perm:
             perms.append(''.join(item))
 
         for word in words:
             if word in perms:
+                foundWords.append(''.join(word))
                 print(word)
 
-        return perms
+        return foundWords
 
     def loadWordsFromFile(self, letters, lenght):
         file = gzip.open(self.WORDS_FILE, "rb")
@@ -26,3 +36,10 @@ class Wordcheck(object):
     def getWords(self, letters, length):
         words = self.loadWordsFromFile(letters, length)
         return self.perm(words, length, letters)
+
+    def printWords(self, letters):
+        words = self.getWords(letters, len(letters))
+        for word in words:
+            self.results = self.results + word + '\n'
+
+        
